5846227253e94b11e71f836c88a8475431681030
Elijah <77475786+elijahisrael@users.noreply.github.com>
2023-11-26 02:49:45 -0500

    Update AudioDataPuller.py

--- AudioDataPuller.py
+++ AudioDataPuller.py
@@ -1,5 +1,7 @@
 import wave
 import pyaudio
+import os
+
 
 Chunk_Size = 1024
 Format = pyaudio.paInt16
@@ -13,8 +15,8 @@ Audio_Stream = P.open(format = Format, channels = Channels, rate = Rate, input =
 print("Start recording:")
 
 Frames = []
-Time = 3
-for _ in range(0, int(Rate / Chunk_Size * Time)):
+Time = 10
+for _ in range(0, int(Rate / Chunk_Size * (Time + 1))):
   data = Audio_Stream.read(Chunk_Size) 
   Frames.append(data)
 
@@ -24,9 +26,18 @@ Audio_Stream.stop_stream()
 Audio_Stream.close()
 P.terminate()
 
-Wave_File = wave.open("Wave_Output.wav", "wb")
+output_directory = os.path.join(os.path.expanduser("~"), "Desktop", "wav_files")
+
+if not os.path.exists(output_directory):
+  os.makedirs(output_directory)
+
+wav_file_path = os.path.join(output_directory, "Wave_Output.wav")
+
+Wave_File = wave.open(wav_file_path, "wb")
 Wave_File.setnchannels(Channels)
 Wave_File.setsampwidth(P.get_sample_size(Format))
 Wave_File.setframerate(Rate)
 Wave_File.writeframes(b''.join(Frames))
 Wave_File.close()
+
+print(f"Audio saved to '{wav_file_path}'.")
